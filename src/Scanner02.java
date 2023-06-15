1f5ab39c228079bb085dd85826e91c690692ba59
Дмитрий <mitrofan198522@gmail.com>
2023-06-15 22:13:06 +0300

    Вывод через printf()

--- src/Scanner02.java
+++ src/Scanner02.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Scanner02 {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int num = in.nextInt();
+        System.out.printf("The number is %d", num);
+        in.close();
     }
 }
