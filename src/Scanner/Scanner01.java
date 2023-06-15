48447492cb75da7ac052a46af65b92e0c2daacfb
Дмитрий <mitrofan198522@gmail.com>
2023-06-15 21:37:50 +0300

    initial commit

---
+++ src/Scanner/Scanner01.java
@@ -0,0 +1,15 @@
+package Scanner;
+
+import java.util.Scanner;
+
+public class Scanner01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = sc.nextInt();
+        System.out.println("The number is " + number);
+    }
+
+
+
+}
